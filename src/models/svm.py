1c5614c057fcb3603cf3a18cf2b0019b6d1d1431
giuliademme <giulia.demme@studenti.unimi.it>
2025-07-02 19:09:20 +0200

    Inizio modello svm

---
+++ src/models/svm.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class SVM():
+    def __init__(self, learning_rate=1e-3, lambda_param):
+        pass
+
+    def fit(self):
+        pass
+
+    def update_weights(self):
+        pass
+
+    def predict(self):
+        pass
+
+
